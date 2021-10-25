af50bf4a5091e993a060eee2fb61ff911ad21d33
6wanted9 <380973907047DKh>
2021-10-25 11:53:22 +0300

    lab4 katanov last upd

--- lab4/task6/Katanov/task6.js
+++ lab4/task6/Katanov/task6.js
@@ -3,6 +3,10 @@ var inclineButton = document.getElementById("incline");
 var underlineButton = document.getElementById("underline");
 var inputArea = document.getElementById("input_area");
 var outputArea = document.getElementById("output_area");
+var colorValue = document.getElementById("color-value");
+var colorButton = document.getElementById("color-button");
+var regValue = document.getElementById("reg-value");
+var regButton = document.getElementById("reg-button");
 
 var text = inputArea.value;
 
@@ -10,8 +14,12 @@ var text = inputArea.value;
 boldButton.addEventListener("click", boldBtn, false);
 inclineButton.addEventListener("click", inclineBtn, false);
 underlineButton.addEventListener("click", underlineBtn, false);
-inputArea.addEventListener("onkeydown", areaUpd, false);
-
+inputArea.addEventListener("input", areaUpd, false);
+colorButton.addEventListener("click", colorBtn, false);
+regButton.addEventListener("click", regBtn, false);
+//
+var lstyle = "";
+var rstyle = "";
 //
 var isBold = false;
 var isInclined = false;
@@ -19,55 +27,74 @@ var isUnderlined = false;
 function areaUpd()
 {
     text = inputArea.value;
+    outputArea.innerHTML = lstyle + text + rstyle;
 }
 function boldBtn()
 {
     if(isBold)
     {
-        text = text.replace("<b>", "");
-        text = text.replace("</b>", "");
+        lstyle = lstyle.replace("<b>", "");
+        rstyle = rstyle.replace("</b>", "");
         boldButton.className = "edit-button";
 
     }
     else
     {
-        text = text.bold();
+        lstyle += "<b>";
+        rstyle += "</b>";
         boldButton.className = "edit-button pressed_button";
     }
-    outputArea.innerHTML = text;
+    outputArea.innerHTML = lstyle + text + rstyle;
     isBold = !isBold;
 }
 function inclineBtn()
 {
     if(isInclined)
     {
-        text = text.replace("<i>", "");
-        text = text.replace("</i>", "");
+        lstyle = lstyle.replace("<i>", "");
+        rstyle = rstyle.replace("</i>", "");
         inclineButton.className = "edit-button";
 
     }
     else
     {
-        text = text.italics();
+        lstyle += "<i>";
+        rstyle += "</i>";
         inclineButton.className = "edit-button pressed_button";
     }
-    outputArea.innerHTML = text;
+    outputArea.innerHTML = lstyle + text + rstyle;
     isInclined = !isInclined;
 }
 function underlineBtn()
 {
     if(isUnderlined)
     {
-        text = text.replace("<b>", "");
-        text = text.replace("</b>", "");
+        lstyle = lstyle.replace("<u>", "");
+        rstyle = rstyle.replace("</u>", "");
         underlineButton.className = "edit-button";
 
     }
     else
     {
-        text = text.underline();
+        lstyle += "<u>";
+        rstyle += "</u>";
         underlineButton.className = "edit-button pressed_button";
     }
-    outputArea.innerHTML = text;
+    outputArea.innerHTML = lstyle + text + rstyle;
     isUnderlined = !isUnderlined;
+}
+function colorBtn()
+{
+    outputArea.style.color = colorValue.value;
+}
+function regBtn()
+{
+    if(regValue.value == "Верхній регістр")
+    {
+        outputArea.style.textTransform = "uppercase";
+    }
+    else if(regValue.value == "Нижній регістр")
+    {
+        outputArea.style.textTransform = "lowercase";
+    }
 }
